9c29a1aac4059952415da5180605b64de3b683b9
felix1805 <felix1805@gmail.com>
2023-05-10 16:32:02 -0400

    finalized styling, defined default search parameters and tested functionality.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# simple-ai-image-generator
-This is a basic image generator written in Javascript with custom CSS styling.
+# Simple-ai-image-generator - Description
+This is a basic image generator written in Javascript with custom CSS styling. It utilizes the OpenAI image API and is setup to search for images with 1024x1024 dimensions. 
+Upon cloning the directory, a user will need to add their own API key on the first line of the "app.js" file.
+Once the repository has been cloned and the API key has been configured, simply start the image generator from the terminal using the command "start index.html"
--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_KEY = '';
+const API_KEY = "**********";
 const submitIcon = document.querySelector("#submit-icon");
 const inputElement = document.querySelector("input");
 const imageSection = document.querySelector(".images-section");
@@ -11,9 +11,9 @@ const getImages = async () => {
       "Content-Type": "application/json"
     },
     body: JSON.stringify({  
-      "prompt": inputElement.value,
-      "n": 4,
-      "size":"1024x1024"
+      prompt: inputElement.value,
+      n: 4,
+      size:"1024x1024"
     })
   }
   try {
@@ -21,10 +21,10 @@ const response = await fetch("https://api.openai.com/v1/images/generations", opt
 const data = await response.json()
 
 data?.data.forEach(imageObject => {
-  const imageContainer = document.createElement('div');
-  imageContainer.classList.add('image-container');
-  const imageElement = document.createElement('img');
-  imageElement.setAttribute('src', imageObject.url);
+  const imageContainer = document.createElement("div");
+  imageContainer.classList.add("image-container");
+  const imageElement = document.createElement("img");
+  imageElement.setAttribute("src", imageObject.url);
   imageContainer.append(imageElement);
   imageSection.append(imageContainer);
 })
@@ -33,4 +33,4 @@ data?.data.forEach(imageObject => {
   }
 }
 
-submitIcon.addEventListener('click', getImages)
+submitIcon.addEventListener("click", getImages)
